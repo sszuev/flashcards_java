6aa6f128d0fc7762aa3be7fbd0e45cb7f5a807e1
sszuev <sss.zuev@gmail.com>
2022-02-12 14:04:22 +0300

    flashcards: add double-click on dictionary row, selection logic, add run button

--- tutor/src/main/resources/static/dictionary.js
+++ tutor/src/main/resources/static/dictionary.js
@@ -2,12 +2,28 @@
  * dictionary js-script library.
  */
 
+const selectedRowClass = 'table-secondary';
+const runRowClass = 'table-dark';
+
 function drawDictionariesPage() {
     $.get('/api/dictionaries').done(function (response) {
         displayPageCard('dictionaries');
 
         const tbody = $('#dictionaries tbody');
+        const thead = $('#dictionaries thead');
+        const title = $('#dictionaries .card-title');
+        const btnRun = $('#btn-run');
         tbody.html('');
+        resetSelection();
+        thead.on('click', function () {
+            resetRowSelections(tbody);
+            resetSelection();
+        });
+        title.on('click', function () {
+            resetRowSelections(tbody);
+            resetSelection();
+        });
+        btnRun.on('click', runStage);
         $.each(response, function (key, value) {
             let row = $(`<tr id="${value.id}">
                             <td>${value.sourceLang}</td>
@@ -16,14 +32,36 @@ function drawDictionariesPage() {
                             <td>${value.total}</td>
                             <td>${value.learned}</td>
                           </tr>`);
-            row.unbind('click').on('click', function () {
+            row.on('click', function () {
                 dictionary = value;
-                $.get('/api/cards/random/' + dictionary.id).done(function (array) {
-                    data = array;
-                    stageShow();
-                });
+                resetRowSelections(tbody);
+                row.addClass(selectedRowClass);
+                btnRun.prop("disabled", false);
             });
+            row.dblclick(runStage);
             tbody.append(row);
         });
+    });
+}
+
+function resetRowSelections(tbody) {
+    $('tr', tbody).each(function (i, r) {
+        $(r).removeClass();
+    })
+}
+
+function resetSelection() {
+    dictionary = null;
+    $('#btn-run').prop("disabled", true);
+}
+
+function runStage() {
+    if (dictionary == null) {
+        return;
+    }
+    $('#' + dictionary.id).addClass(runRowClass);
+    $.get('/api/cards/random/' + dictionary.id).done(function (array) {
+        data = array;
+        stageShow();
     });
 }
